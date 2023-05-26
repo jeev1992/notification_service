3c0f7e4a0a8a7cf741701d3b7e306eb07c7f2510
Jeevendra Singh <jeesingh@microsoft.com>
2023-05-26 22:24:30 +0530

    bg job

--- crons/emailSenderBackgroundJob.js
+++ crons/emailSenderBackgroundJob.js
@@ -1,10 +1,37 @@
 const cron = require('node-cron')
+const TicketNotificationModel = require('../models/ticketNotification.model')
+const EmailTransporter = require('../notifier/emailService')
 
 cron.schedule('*/30 * * * * *', async () => {
-    //console.log("I am background job")
+    console.log("I am background job")
 
-    //Query all the notification documents whose status is NOT_SENT
+    //Query all the notification documents which are in status "NOT_SENT"
 
-    //Loop over the query result and send out email using the transport object
-    //Mark the status of notification as senr
+    const notifications = await TicketNotificationModel.find({
+        status: "NOT_SENT"
+    })
+
+    if(notifications && notifications.length > 0){
+        notifications.forEach(notification => {
+            const mailData = {
+                from: "crmtestuser100@gmail.com",
+                to: notification.recepientEmails,
+                subject: notification.subject,
+                text: notification.content
+            }
+
+            //Send the email
+            EmailTransporter.sendMail(mailData, async function (err, info) {
+                if(err){
+                    console.log(err.message)
+                }else{
+                    console.log(info)
+
+                    //Update the notification document status
+                    notification.status = "SENT"
+                    await notification.save()
+                }
+            })
+        })
+    }
 })
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json())
+require('./crons/emailSenderBackgroundJob')
 
 
 //Establish DB connection
